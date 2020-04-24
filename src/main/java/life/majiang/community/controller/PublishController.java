34fe5e19fdfaaa3c460b0ba2eb0d6ce216107380
rwchao <renwc0110@outlook.com>
2020-04-24 20:14:15 +0800

    add standized tag library

--- src/main/java/life/majiang/community/controller/PublishController.java
+++ src/main/java/life/majiang/community/controller/PublishController.java
@@ -1,10 +1,12 @@
 package life.majiang.community.controller;
 
+import life.majiang.community.cache.TagCache;
 import life.majiang.community.dto.QuestionDTO;
 import life.majiang.community.mapper.QuestionMapper;
 import life.majiang.community.model.Question;
 import life.majiang.community.model.User;
 import life.majiang.community.service.QuestionService;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -30,11 +32,14 @@ public class PublishController {
         model.addAttribute("description",question.getDescription());
         model.addAttribute("tag",question.getTag());
         model.addAttribute("id", question.getId());
+
+        model.addAttribute("tags", TagCache.get());
         return "publish";
     }
 
     @GetMapping("/publish")
-    public String publish(){
+    public String publish(Model model){
+        model.addAttribute("tags", TagCache.get());
         return "publish";
     }
 
@@ -49,15 +54,21 @@ public class PublishController {
         model.addAttribute("title",title);
         model.addAttribute("description",description);
         model.addAttribute("tag",tag);
-
+        model.addAttribute("tags", TagCache.get());
         if(title==null || title==""){
-            model.addAttribute("title","标题不能为空");
+            model.addAttribute("error","标题不能为空");
         }
         if(description==null || description==""){
-            model.addAttribute("description","问题补充不能为空");
+            model.addAttribute("error","问题补充不能为空");
         }
         if(tag==null || tag==""){
-            model.addAttribute("tag","标签不能为空");
+            model.addAttribute("error","标签不能为空");
+        }
+
+        String invalid = TagCache.filterInvalid(tag);
+        if(StringUtils.isNoneBlank(invalid)){
+            model.addAttribute("error","输入非法标签:"+invalid);
+            return "publish";
         }
 
         User user = (User)request.getSession().getAttribute("user");
